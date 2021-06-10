b45d53ca93038dce777950295d53fd205ee93d4b
sne-syn <snezhana.syngur@gmail.com>
2021-06-10 16:25:13 +0200

    Update js classes

--- app/assets/javascripts/flip-card.js
+++ app/assets/javascripts/flip-card.js
@@ -1,9 +1,9 @@
 document.addEventListener('turbolinks:load', function () {
-  const flipButtons = document.querySelectorAll('.flip-btn');
+  const flipButtons = document.querySelectorAll('.jsFlipBtn');
 
   if (flipButtons.length > 0) {
     flipButtons.forEach(btn => {
-      const flipper = btn.closest('.flipper');
+      const flipper = btn.closest('.jsFlipper');
       btn.addEventListener('click', () => {
         flipper.classList.toggle('flip');
       });
