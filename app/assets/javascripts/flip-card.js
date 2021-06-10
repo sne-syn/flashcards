e30a3b095487c98bcd9cbf75760255167fa26c46
sne-syn <snezhana.syngur@gmail.com>
2021-06-10 14:39:29 +0200

    Fix lint errors

--- app/assets/javascripts/flip-card.js
+++ app/assets/javascripts/flip-card.js
@@ -1,12 +1,16 @@
-document.addEventListener('turbolinks:load', function () {
-  const flipBtn = document.querySelectorAll('.flip-btn');
-  
-  if (flipBtn.length > 0) {
-    flipBtn.forEach(function (btn) {
-      const flipper = btn.closest('.flipper');
-      btn.addEventListener('click', function () {
-        flipper.classList.toggle('flip');
-      });
-    })
-  }
-})
+'use strict';
+
+(function () {
+  document.addEventListener('turbolinks:load', function () {
+    var flipBtn = document.querySelectorAll('.flip-btn');
+
+    if (flipBtn.length > 0) {
+      flipBtn.forEach(function (btn) {
+        var flipper = btn.closest('.flipper');
+        btn.addEventListener('click', function () {
+          flipper.classList.toggle('flip');
+        });
+      })
+    }
+  })
+})();
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
   def disable_link_to_current_page(link_path)
-    current_page?(link_path) ? "" : "#{link_path}"
+    current_page?(link_path) ? '' : link_path.to_s
   end
 
   def add_active_class(link_path)
-    current_page?(link_path) ? "d-none" : ""
+    current_page?(link_path) ? 'd-none' : ''
   end
 end
