b0d964df57e1eebdd9c5b9c23ab0bfd0d4e61fec
sne-syn <snezhana.syngur@gmail.com>
2021-06-15 23:15:37 +0200

    wip: create a new card

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -2,4 +2,23 @@ class CardsController < ApplicationController
   def index
     @cards = Card.all
   end
+
+  def new
+    @card = Card.new
+  end
+
+  def create
+    @card = Card.new(card_params)
+
+    if @card.save
+      redirect_to @card
+    else
+      render :new
+    end
+  end
+
+  private
+    def card_params
+      params.require(:card).permit(:original_text, :translated_text, :review_date)
+    end
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,6 +1,5 @@
 class Card < ApplicationRecord
   validates :original_text, presence: true
-  validates :translated_text, presence: true, exclusion: { in: lambda{ |card| [card.original_text] }, 'The value must be different from the one used in the original text' }
+  validates :translated_text, presence: true, exclusion: { in: lambda{ |card| [card.original_text] }}
   validates :review_date, presence: true
-
 end
