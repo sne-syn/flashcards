d1842e5f1a2b89df52816f228105d6cc323e46c6
sne-syn <snezhana.syngur@gmail.com>
2021-06-17 14:38:43 +0200

    Small updates

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,7 +2,7 @@ class Card < ApplicationRecord
   before_save :set_review_date 
 
   validates :original_text, presence: true
-  validates :translated_text, presence: true, exclusion: { in: lambda{ |card| [card.original_text] }, message: 'Перевод не должен повторять слово для изучения'}
+  validates :translated_text, presence: true, exclusion: { in: lambda{ |card| [card.original_text] }, message: 'не должен повторять слово для изучения'}
 
   protected
   def set_review_date 
