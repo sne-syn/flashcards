33393ae967db86951807ac87beba0e399988d92a
sne-syn <snezhana.syngur@gmail.com>
2021-06-17 14:15:53 +0200

    Add actions, style edit/delete links

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,5 +1,11 @@
 class Card < ApplicationRecord
+  before_save :set_review_date 
+
   validates :original_text, presence: true
-  validates :translated_text, presence: true, exclusion: { in: lambda{ |card| [card.original_text] }}
-  validates :review_date, presence: true
+  validates :translated_text, presence: true, exclusion: { in: lambda{ |card| [card.original_text] }, message: 'Перевод не должен повторять слово для изучения'}
+
+  protected
+  def set_review_date 
+    self.review_date = Time.now + 3.days
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'homepage#index'
 
-  resources :cards
+  resources :cards, only: [:index, :new, :create, :edit, :update, :destroy]
 end
