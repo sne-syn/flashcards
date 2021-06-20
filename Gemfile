ec801f9fdb2c20cdf7ce904b87545269472a1b54
sne-syn <snezhana.syngur@gmail.com>
2021-06-20 20:25:11 +0200

    Basic data scraping with css

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ gem 'bootsnap', '>= 1.1.0', require: false
 gem 'haml-rails', '~> 2.0'
 gem 'rails-i18n'
 gem 'simple_form'
+gem 'nokogiri'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'nokogiri'
+require 'open-uri'
+
+class Scraper
+  def scrape_wordlist
+    studynow_url = 'https://studynow.ru/dicta/allwords'
+    html = open(studynow_url)
+    doc = Nokogiri::HTML(html)
+    data_node_set = doc.css('#wordlist').css('tr')
+    cards = []
+
+    data_node_set.each do |node|
+      original_text = node.css('td[2]').text
+      translated_text = node.css('td[3]').text
+      card = {
+        original_text: original_text,
+        translated_text: translated_text
+      }
+
+      cards << card
+    end
+
+    cards
+    puts (cards)
+  end
+end
+
+scrape = Scraper.new
+scrape.scrape_wordlist
