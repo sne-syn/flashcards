c0e879a5af38527182a6727542e034c5e9c8f6d3
sne-syn <snezhana.syngur@gmail.com>
2021-06-23 21:28:23 +0200

    Add some tests

--- spec/features/new_card_page_spec.rb
+++ spec/features/new_card_page_spec.rb
@@ -5,7 +5,26 @@ describe 'New Card Page' do
     visit '/cards/new'
   end
 
+  let (:card) { build(:card) }
+
   it 'should have the correct heading' do
     expect(page).to have_content(I18n.t('new_card.heading'))
   end
+  
+  it 'adds a new card to the cards list' do
+    fill_in 'card[original_text]', with: card.original_text
+    fill_in 'card[translated_text]', with: card.translated_text
+    click_button I18n.t('form.create_card_btn')
+    expect(page).to have_content(card.original_text)
+  end
+
+  it 'clears inputs on cancel btn click and rerender form' do
+    fill_in 'card[original_text]', with: card.original_text
+    fill_in 'card[translated_text]', with: card.translated_text
+    click_link I18n.t('form.cancel_btn')
+    
+    expect(page).to have_content(I18n.t('new_card.heading'))
+    expect(page).not_to have_content(card.original_text)    
+  end
+    
 end
