5ad47e4a95138bcb8075430a90b0e10ea6fa49cf
sne-syn <snezhana.syngur@gmail.com>
2021-06-23 18:16:20 +0200

    Add test with vcr

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,10 @@
 require 'csv'
+require_relative '../lib/scraper'
 
 Card.destroy_all
 
-csv_text = File.read('lib/seeds/scraped_cards_data.csv')
-csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
-cards = []
-
-csv.each do |row|
-  cards << row.to_hash
-end
+scrape = Scraper.new
+cards = scrape.get_data(100)
 
 cards.each do |card|
   CreateCardService.call(card)
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,8 @@ require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'vcr'
+require 'support/factory_bot'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -30,10 +32,17 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
+VCR.configure do |config|
+  config.cassette_library_dir = "spec/support/vcr_cassettes"
+  config.hook_into :webmock
+  config.ignore_localhost = true
+  config.configure_rspec_metadata!
+end
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
@@ -62,3 +71,4 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
+
